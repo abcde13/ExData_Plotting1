cfa0c33ff2944ea339e53f034096f97de762396d
Joraaver Chahal <machspeeds@gmail.com>
2014-08-07 15:38:40 -0700

    Couldn't just source data creation file

--- plot4.R
+++ plot4.R
@@ -4,7 +4,24 @@
 # Plot4                 #
 #########################
 
-source("ElectricPowerConsumptionAnalysis.R")
+#source("ElectricPowerConsumptionAnalysis.R")
+#Can't just source, per instructions
+
+# First, get colnames
+col_names <- colnames(read.table("household_power_consumption.txt",sep=";",header=T,
+                                 nrow=2))
+
+# First Skip 66636 , stop at 69515 (calculated at terminal)
+to_skip <- 66636
+to_read <- 69515-66636
+
+# Read data given skip and rows now
+data <- read.table("household_power_consumption.txt",header=T,sep=";",skip=to_skip,
+                   nrows=to_read+1,col.names=col_names,na.strings = "?")
+
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+data$Time <- apply(data,1,function(x) { paste(x[1],x[2])})
+data$Time <- strptime(data$Time,"%Y-%m-%d %H:%M:%S")
 
 
 par(mfrow = c(2,2))
